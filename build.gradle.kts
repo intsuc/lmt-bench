d7ca1075a379737ea0b4124296ac7e109fbce681
intsuc <intsuc@gmail.com>
2023-06-07 07:26:20 +0900

    Add datapack generator

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
   kotlin("jvm") version "1.9.0-Beta"
+  kotlin("plugin.serialization") version "1.9.0-Beta"
+  application
 }
 
 repositories {
@@ -7,8 +9,13 @@ repositories {
 }
 
 dependencies {
+  implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 }
 
 kotlin {
   jvmToolchain(19)
+}
+
+application {
+  mainClass.set("MainKt")
 }
---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,82 @@
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.encodeToStream
+import java.util.zip.ZipEntry
+import java.util.zip.ZipOutputStream
+import kotlin.io.path.Path
+import kotlin.io.path.createDirectories
+import kotlin.io.path.outputStream
+
+@OptIn(ExperimentalSerializationApi::class)
+fun main(args: Array<String>) {
+  args.forEach { println(it) }
+
+  Path("mch-config.json").outputStream().buffered().use {
+    Json.encodeToStream(
+      MchConfig(
+        5,
+        5,
+        10,
+        5,
+        "ns",
+        null,
+        null,
+        null,
+        null,
+        listOf("bench")
+      ),
+      it,
+    )
+  }
+
+  ZipOutputStream(
+    Path("world", "datapacks", "lmt.zip")
+      .createDirectories()
+      .outputStream()
+      .buffered()
+  ).use {
+    it.putNextEntry(ZipEntry("pack.mcmeta"))
+    Json.encodeToStream(PackMetadata(PackMetadata.PackMetadataSection("", 15)), it)
+
+    it.putNextEntry(ZipEntry("data/mch/tags/functions/setup.json"))
+    Json.encodeToStream(TagFile(listOf("setup")), it)
+
+    it.putNextEntry(ZipEntry("data/minecraft/functions/setup.mcfunction"))
+    it.write("""say setup""".encodeToByteArray())
+
+    it.putNextEntry(ZipEntry("data/minecraft/functions/bench.mcfunction"))
+    it.write("""seed""".encodeToByteArray())
+  }
+}
+
+@Serializable
+data class MchConfig(
+  @SerialName("warmup_iterations") val warmupIterations: Int?,
+  @SerialName("measurement_iterations") val measurementIterations: Int?,
+  val time: Int?,
+  val forks: Int?,
+  @SerialName("time_unit") val timeUnit: String?,
+  val mc: String?,
+  @SerialName("jvm_args") val jvmArgs: List<String>?,
+  @SerialName("mc_args") val mcArgs: List<String>?,
+  @SerialName("parsing_benchmarks") val parsingBenchmarks: List<String>?,
+  @SerialName("execute_benchmarks") val executeBenchmarks: List<String>?,
+)
+
+@Serializable
+data class PackMetadata(
+  val pack: PackMetadataSection,
+) {
+  @Serializable
+  data class PackMetadataSection(
+    val description: String,
+    @SerialName("pack_format") val packFormat: Int,
+  )
+}
+
+@Serializable
+data class TagFile(
+  val values: List<String>,
+)
